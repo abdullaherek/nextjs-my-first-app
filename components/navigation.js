5f4053fdff95e3a7a586382ee21c048375903f24
abdullaherek <abdullah.erek@ogrenci.bartin.edu.tr>
2021-09-24 17:33:29 +0300

    first commit

---
+++ components/navigation.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import styles from './nav.module.css';
+
+function Navigation() {
+    return <nav>
+    <Link href="/" >
+      <a className={styles.link}> Ana Sayfa</a>
+    </Link>
+    <Link href="/about">
+      <a> Hakkımızda</a>
+    </Link>
+    </nav>
+}
+
+export default Navigation
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout";
+
+function HomePage() {
+  return (
+    <Layout>
+                    <title>Hakkımda</title>
+
+      <h1>About sayfası</h1>
+    </Layout>
+  );
+}
+
+export default HomePage;
